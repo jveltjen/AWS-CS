db6e26ddf48ac8b3534330e40a440c6a3e0d12fd
jveltjen <30809952+jveltjen@users.noreply.github.com>
2022-02-06 18:35:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,32 @@ provider "aws" {
   region     = "eu-central-1"
 }
 
+
+resource "aws_security_group" "access_ssh" {
+  name = "ssh-terraform"
+
+  #Incoming traffic
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["213.211.183.93/32"] #replace it with your ip address
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "i-amazon-linux-prod" {
   ami                         = "ami-030e490c34394591b"
   instance_type               = "t2.micro"
   key_name                    = "aws-eucentral-linux"
-  vpc_security_group_ids = ["sg-0c224099ae515e7fa"]
+  vpc_security_group_ids = ["ssh-terraform"]
   iam_instance_profile = "FA-SSM"
   user_data = <<EOF
 #!/bin/bash
@@ -25,7 +46,7 @@ resource "aws_instance" "i-redhat" {
   ami                         = "ami-06ec8443c2a35b0ba"
   instance_type               = "t2.micro"
   key_name                    = "aws-eucentral-linux"
-  vpc_security_group_ids = ["sg-0c224099ae515e7fa"]
+  vpc_security_group_ids = ["ssh-terraform"]
   iam_instance_profile = "FA-SSM"
   user_data = <<EOF
 #!/bin/bash
@@ -43,7 +64,7 @@ resource "aws_instance" "i-redhat2" {
   ami                         = "ami-06ec8443c2a35b0ba"
   instance_type               = "t2.micro"
   key_name                    = "aws-eucentral-linux"
-  vpc_security_group_ids = ["sg-0c224099ae515e7fa"]
+  vpc_security_group_ids = ["ssh-terraform"]
   iam_instance_profile = "FA-SSM"
   user_data = <<EOF
 #!/bin/bash
